216a76e0c2942e9039434e745fecf5c9aaaf45fa
debmalyap <debmalyapaulfbp@gmail.com>
2025-12-11 14:23:15 +0530

    Updated docker compose file and added init-scripts file

---
+++ init-scripts/01-init.sql
@@ -0,0 +1,11 @@
+-- Create schema
+CREATE SCHEMA IF NOT EXISTS spring_boot_tech;
+
+
+CREATE TABLE IF NOT EXISTS spring_boot_tech.erp_st_bank_master (
+    bank_master_id VARCHAR(250) PRIMARY KEY,
+    bank_code VARCHAR(100) NOT NULL,
+    bank_name VARCHAR(200) UNIQUE,
+    ifsc_code VARCHAR(50) UNIQUE,
+    bank_address VARCHAR(250) UNIQUE,
+);
