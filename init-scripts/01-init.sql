c7cde533f6324a78fb9adee15e645589ced33482
debmalyap <debmalyapaulfbp@gmail.com>
2025-12-11 14:34:01 +0530

    Sql file updated

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -1,11 +1,11 @@
 -- Create schema
 CREATE SCHEMA IF NOT EXISTS spring_boot_tech;
 
-
+-- Create table
 CREATE TABLE IF NOT EXISTS spring_boot_tech.erp_st_bank_master (
     bank_master_id VARCHAR(250) PRIMARY KEY,
     bank_code VARCHAR(100) NOT NULL,
-    bank_name VARCHAR(200) UNIQUE,
-    ifsc_code VARCHAR(50) UNIQUE,
-    bank_address VARCHAR(250) UNIQUE,
+    bank_name VARCHAR(200),
+    ifsc_code VARCHAR(50),
+    bank_address VARCHAR(250)
 );
